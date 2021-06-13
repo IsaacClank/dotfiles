11e02068d6d7fd155c5539b36786388127d90c68
Isaac Clank <minimaleffort769@gmail.com>
2021-06-13 09:57:56 +0700

    Add dotfiles for bashit

---
+++ bash/bobby.theme.bash
@@ -0,0 +1,35 @@
+# shellcheck shell=bash
+
+SCM_THEME_PROMPT_DIRTY=" ${red}✗"
+SCM_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+SCM_THEME_PROMPT_PREFIX=" ${green}("
+SCM_THEME_PROMPT_SUFFIX="${green})"
+
+GIT_THEME_PROMPT_DIRTY=" ${red}✗"
+GIT_THEME_PROMPT_CLEAN=" ${bold_green}✓"
+GIT_THEME_PROMPT_PREFIX=" ${purple}("
+GIT_THEME_PROMPT_SUFFIX="${purple})"
+
+RVM_THEME_PROMPT_PREFIX="|"
+RVM_THEME_PROMPT_SUFFIX="|"
+
+__bobby_clock() {
+	printf '%s' "$(clock_prompt) "
+
+	if [ "${THEME_SHOW_CLOCK_CHAR}" == "true" ]; then
+		printf '%s' "$(clock_char) "
+	fi
+}
+
+function prompt_command() {
+	PS1="\n${bold_cyan}\w\n${reset_color}"
+	PS1+="${bold_purple}$(scm_prompt_char_info) "
+	PS1+="${reset_color}→ "
+}
+
+THEME_SHOW_CLOCK_CHAR=${THEME_SHOW_CLOCK_CHAR:-"true"}
+THEME_CLOCK_CHAR_COLOR=${THEME_CLOCK_CHAR_COLOR:-"$red"}
+THEME_CLOCK_COLOR=${THEME_CLOCK_COLOR:-"$bold_cyan"}
+THEME_CLOCK_FORMAT=${THEME_CLOCK_FORMAT:-"%Y-%m-%d %H:%M:%S"}
+
+safe_append_prompt_command prompt_command
